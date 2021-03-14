9f14c0021456eb8953e0eb270fc16e0ce06b05af
meinu-chan <62944053+meinu-chan@users.noreply.github.com>
2021-03-14 16:57:58 +0200

    bug fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require("./app");
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
-app.listen(PORT, () => console.log(`Server has been started: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server has been started: ${PORT}`));
