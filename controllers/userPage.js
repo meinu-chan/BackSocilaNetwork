2b2d908a5607cecbfa12504bafa5c7eb19115ae3
meinu-chan <62944053+meinu-chan@users.noreply.github.com>
2021-03-22 14:44:42 +0200

    user response waitingForResponse as Obj && friends as Obj[]

--- controllers/userPage.js
+++ controllers/userPage.js
@@ -2,22 +2,44 @@ const User = require("../models/UserModel")
 const errorHandler = require("../utils/errorHandler")
 const _ = require("lodash")
 
-module.exports.findById = async (req, res) => {
+const getFriendsObj = async ({ publications, requests, waitingForResponse, _id, nickname, friends }) => {
+    const friendsArr = await Promise.all(friends.map(async (friendId) => {
+        const friend = await User.findById(friendId)
+        return { friendId, friendNickname: friend.nickname }
+    }))
+
+    const waitingForResponseArr = await Promise.all(waitingForResponse.map(async (waiterId) => {
+        const waiter = await User.findById(waiterId)
+        return { waiterId, waiterNickname: waiter.nickname }
+    }))
+
+    return { publications, requests, waitingForResponse: waitingForResponseArr, _id, nickname, friends: friendsArr }
+}
+
+module.exports.findById = (req, res) => {
     const { params: { id } } = req
     try {
-        const userId = id.replace("id=", "")
+        const userPromise = new Promise(async (resolve, reject) => {
+            const user = await User.findById({ _id: id })
+            const { publications, requests, waitingForResponse, _id, nickname, friends } = user
 
-        const user = await User.findById({ _id: userId }).catch(err => console.log(err))
+            resolve({ publications, requests, waitingForResponse, _id, nickname, friends })
+        })
 
-        if (user) {
-            res.status(200).json({
-                user
+        userPromise.then(user => getFriendsObj(user))
+            .then(user => {
+                // console.log(user)
+                res.status(200).json({
+                    user
+                })
             })
-        } else {
-            res.status(404).json({
-                message: "User doesn't exist."
+            .catch((error) => {
+                res.status(404).json({
+                    message: error.message ? error.message : error
+                })
+
             })
-        }
+
     }
     catch (error) {
         res.status(500).json({
@@ -31,9 +53,27 @@ module.exports.findByName = async (req, res) => {
     const { params: { nickname } } = req
     try {
         const userNickname = nickname.replace("nickname=", "")
-        const user = await User.findOne({ nickname: userNickname })
-        user ? res.status(200).json({ user }) :
-            res.status(404).json({ message: "User didn't found." })
+        const userPromise = new Promise(async (resolve, reject) => {
+            const user = await User.findOne({ nickname: userNickname })
+            const { publications, requests, waitingForResponse, _id, nickname, friends } = user
+
+            resolve({ publications, requests, waitingForResponse, _id, nickname, friends })
+        })
+
+        userPromise.then(user => getFriendsObj(user))
+            .then(user => {
+                // console.log(user)
+                res.status(200).json({
+                    user
+                })
+            })
+            .catch((error) => {
+                res.status(404).json({
+                    message: error.message ? error.message : error
+                })
+
+            })
+
 
     } catch (error) {
         errorHandler(error, res)
@@ -63,7 +103,11 @@ module.exports.addFriend = async (req, res) => {
         await user.updateOne({ waitingForResponse })
         _.remove(requests, (id) => id == user._id)
         await newFriend.updateOne({ requests })
-        res.status(200).json({ user })
+        getFriendsObj(user).then((data) => {
+            res.status(200).json(
+                { ...data }
+            )
+        }).catch(err => errorHandler(err, res))
 
     } else {
         res.status(404).json({
@@ -109,7 +153,7 @@ module.exports.sendRequest = async (req, res) => {
     const { body: { userId }, user } = req
 
     const potentialFriend = await User.findById({ _id: userId })
-
+    console.log(user)
     if (potentialFriend && user) {
         try {
             const { waitingForResponse } = potentialFriend
@@ -127,9 +171,12 @@ module.exports.sendRequest = async (req, res) => {
                 await potentialFriend.updateOne({ waitingForResponse })
             }
 
-            res.status(200).json({
-                potentialFriend,
-            })
+            getFriendsObj(potentialFriend).then((data) => {
+                res.status(200).json(
+                    { ...data }
+                )
+            }).catch(err => errorHandler(err, res))
+
 
         } catch (error) {
             errorHandler(error, res)
@@ -156,9 +203,12 @@ module.exports.deleteFriend = async (req, res) => {
                 _.remove(u_friends, (id) => id == f_id.toString())
                 await user.updateOne({ friends: u_friends })
 
-                res.status(200).json({
-                    ex_friend: ex_friend.friends
-                })
+                getFriendsObj(ex_friend).then((data) => {
+                    res.status(200).json(
+                        { ...data }
+                    )
+                }).catch(err => errorHandler(err, res))
+
             } catch (error) {
                 errorHandler(error, res)
             }
--- routes/userPage.js
+++ routes/userPage.js
@@ -4,10 +4,10 @@ const passport = require("passport");
 const router = express.Router();
 
 // localhost:5000/api/page/find/id=123
-router.get("/find/:id", controller.findById)
+router.get("/find/id=:id", controller.findById)
 
 // localhost:5000/api/page/find/nickname=123
-router.get("/find/name/:nickname", passport.authenticate("jwt", { session: false }), controller.findByName)
+router.get("/find/:nickname", passport.authenticate("jwt", { session: false }), controller.findByName)
 
 // localhost:5000/api/page/friends/add
 router.put("/friends/add", passport.authenticate("jwt", { session: false }), controller.addFriend)
