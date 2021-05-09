80c104ba9c1aa9059ec3823462f9e25f397fa941
Aleksey Lenchenko <lenchenko.aleksey@pettersonapps.com>
2021-05-09 12:35:15 +0300

    comment get by id fix

--- routes/comment.js
+++ routes/comment.js
@@ -3,10 +3,10 @@ const controller = require("../controllers/comment")
 const passport = require("passport");
 const router = express.Router();
 
-// localhost:5000/api/comment/addComment
-router.put("/addComment", passport.authenticate("jwt", { session: false }), controller.addComment)
+// localhost:5000/api/comment/getComment
+router.get("/getComment", controller.getComment)
 
 // localhost:5000/api/comment/addComment
-router.post("/getComment", passport.authenticate("jwt", { session: false }), controller.getComment)
+router.put("/addComment", passport.authenticate("jwt", { session: false }), controller.addComment)
 
 module.exports = router
